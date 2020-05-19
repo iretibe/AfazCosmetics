932ecabe20dbac93c2ddf58488a463b8fa130324
Somad Y <iretibe@users.noreply.github.com>
2020-05-19 19:06:00 +0100

    Basics of Angular

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Afaz Cosmetique';
+  // products: any[];
+  products: IProduct[];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (response: IPagination) => {
+        this.products = response.data;
+      // (response: any) => {
+        // this.products = response.data;
+        // console.log(response);
+      },
+      error => {
+        console.log(error);
+      });
+  }
 }
---
+++ Client/src/demo.ts
@@ -0,0 +1,29 @@
+let data: number | string;
+
+data = 42;
+data = '42';
+
+interface ICar {
+    color: string;
+    model: string;
+    topSpeed?: number;
+}
+
+const car1: ICar = {
+    color: 'blue',
+    model: 'bmw'
+};
+
+const car2: ICar = {
+    color: 'red',
+    model: 'mercedes',
+    topSpeed: 100
+};
+
+// const multiply = (x: number, y: number): number => {
+//     return x * y;
+// };
+
+const multiply = (x: number, y: number): string => {
+    return (x * y).toString();
+};
