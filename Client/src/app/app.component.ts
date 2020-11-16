e3dd043580dcca37de25bc05f7ed54e5adbc3991
user <user@DESKTOP-DT4JVNJ>
2020-11-16 07:39:39 +0000

    finished shopping page

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,29 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Afaz Cosmetique';
 
-  constructor(){}
+  constructor(private meta: Meta){
+    let i  = 0;
+    let tim = setInterval(() => {
+
+        let tag = this.meta.getTag('http-equiv=Content-Security-Policy');
+        
+        if (tag) {
+
+          this.meta.removeTag('http-equiv=Content-Security-Policy');
+          let content = tag.getAttribute('content');
+          let str = 'connect-src ';debugger;
+          let index = content.indexOf(str);
+          content = content.slice(0, index + str.length) + "https://baseurl22/ https://baseurl23/ https://baseurl34/ " + content.slice(index + str.length);
+            this.meta.updateTag({ 'http-equiv': 'Content-Security-Policy', content: content });
+        } else {
+
+          this.meta.addTag({ 'http-equiv': 'Content-Security-Policy', content: 'connect-src \'self\' https://baseurl1/ https://baseurl2/ https://baseurl3/;' });
+        }
+
+        if (i == 1) clearInterval(tim);
+        i++;
+    }, 1000);
+  }
 
   ngOnInit(): void {}
 }
--- Client/src/app/shop/shop.component.ts
+++ Client/src/app/shop/shop.component.ts
@@ -74,11 +74,13 @@ export class ShopComponent implements OnInit {
 
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
@@ -88,13 +90,17 @@ export class ShopComponent implements OnInit {
   }
 
   onPageChanged(event: any) {
-    // this.shopParams.pageNumber = event.page;
-    this.shopParams.pageNumber = event;
-    this.getProducts();
+    if(this.shopParams.pageNumber !== event) 
+    {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+    // this.shopParams.pageNumber = event.page;    
   }
 
   onSearch() {
     this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
